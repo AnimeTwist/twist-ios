9226e32661e1c1c888d3f159f190b28292d7cbc4
Thoralf Thelle <thoralf21@gmail.com>
2019-06-17 19:22:36 +0200

    Initial Commit

---
+++ Twister/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twister
+//
+//  Created by Thoralf Thelle on 17/06/2019.
+//  Copyright © 2019 twist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
